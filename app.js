8e089d41d76aecbd074fa50ba674dee8d575c85b
lanaalsoufi <lanaalsoufi@outlook.com>
2021-04-01 15:06:56 +0300

    app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+alert('Hello Everyone');
+    
+    
+
+
+
+
+
+var mathoperation=prompt('Please inter a Username');
+document.write(mathoperation);
+
+console.log(mathoperation);
+
+if(mathoperation=='LANA ALSOUFI'){
+alert(' No Welcome for me');
+}else{
+alert('WELCOME');
+}
+
+var summation= prompt('5+3');
+document.write('5+3=',summation);
+
+var number=prompt('5*3');
+document.write('5*3=',number);
+
