43d9c7e21df8156bf54aa357848cdc458089a043
lanaalsoufi <lanaalsoufi@outlook.com>
2021-04-01 13:46:38 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+alert('Hello Everyone');
+    
+    
+var Username='LANA ALSOUFI';
+alert(Username);
+
+document.write(Username);
+
+var mathoperation=prompt('Please inter a Username');
+
+console.log(Username);
+
+if(mathoperation=='LANA ALSOUFI'){
+alert('Welcome');
+}else{
+alert('NO WELCOME FOR YOU ');
+}
+
+alert(5+3);
+alert(5**3);
+alert(5*3);
